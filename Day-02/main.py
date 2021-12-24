1350b734e321a63bee4a86d5809a6e7ccd8420ab
Pat-Francis <github@pmfrancis.com>
2021-12-24 22:09:14 +0000

    Day 2 completed

--- Day-02/main.py
+++ Day-02/main.py
@@ -10,5 +10,28 @@ def part_one(file: str) -> int:
         return product
 
 
+def part_two(file: str) -> int:
+    with open(file) as f:
+        instructions = [[j, int(k)] for j, k in [i.split() for i in f.read().splitlines()]]
+
+        horizontal = 0
+        depth = 0
+        aim = 0
+
+        for step in instructions:
+            if step[0] == "forward":
+                horizontal += step[1]
+                if aim != 0:
+                    depth += step[1] * aim
+            elif step[0] == "down":
+                aim += step[1]
+            else:
+                aim -= step[1]
+
+        product = horizontal * depth
+        return product
+
+
 input_file = "./input.txt"
 print(f"Part One: {part_one(input_file)}")
+print(f"Part Two: {part_two(input_file)}")
