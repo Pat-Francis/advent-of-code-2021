4817e0211879ccfeb91726a8e53a2124b389858b
Pat-Francis <github@pmfrancis.com>
2021-12-24 20:46:33 +0000

    Day 1 completed

--- Day-01/main.py
+++ Day-01/main.py
@@ -1,11 +1,23 @@
 def part_one(file: str) -> int:
     with open(file) as f:
-        count = 0
         depths = [int(i) for i in f.readlines()]
-        for i in range(len(depths) - 1):
-            if depths[i + 1] > depths[i]:
-                count += 1
-        return count
+        return sliding_window(depths, 1)
 
 
-print(part_one("input.txt"))
+def part_two(file: str) -> int:
+    with open(file) as f:
+        depths = [int(i) for i in f.readlines()]
+        return sliding_window(depths, 3)
+
+
+def sliding_window(numbers: list[int], window: int) -> int:
+    count = 0
+    for i in range(len(numbers) - window):
+        if numbers[i + window] > numbers[i]:
+            count += 1
+    return count
+
+
+input_file = "./input.txt"
+print(f"Part One: {part_one(input_file)}")
+print(f"Part One: {part_two(input_file)}")
