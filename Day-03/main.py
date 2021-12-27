3109dc987dbd97317c6e8a4785adc825cd1e1051
Pat-Francis <github@pmfrancis.com>
2021-12-27 14:56:00 +0000

    Day 3 completed

--- Day-03/main.py
+++ Day-03/main.py
@@ -13,5 +13,30 @@ def part_one(file: str) -> int:
         return product
 
 
+def part_two(file: str) -> int:
+    with open(file) as f:
+        data = f.read().splitlines()
+        oxygen_rating = data
+        co2_rating = data
+
+        for i in range(len(data[0])):
+            oxy_digits = [d[i] for d in oxygen_rating]
+            most_common = '0' if oxy_digits.count('0') > oxy_digits.count('1') else '1'
+            oxygen_rating = [j for j in oxygen_rating if j[i] == most_common]
+            if len(oxygen_rating) == 1:
+                break
+
+        for i in range(len(data[0])):
+            co2_digits = [d[i] for d in co2_rating]
+            most_common = '0' if co2_digits.count('0') > co2_digits.count('1') else '1'
+            co2_rating = [j for j in co2_rating if j[i] != most_common]
+            if len(co2_rating) == 1:
+                break
+
+        product = int(oxygen_rating[0], 2) * int(co2_rating[0], 2)
+        return product
+
+
 input_file = "./input.txt"
 print(f"Part One: {part_one(input_file)}")
+print(f"Part Two: {part_two(input_file)}")
