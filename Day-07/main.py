0414872115836d4cdc89ba6bec167ce3f456ee42
Pat-Francis <github@pmfrancis.com>
2022-01-01 16:49:16 +0000

    Day 7 completed

--- Day-07/main.py
+++ Day-07/main.py
@@ -1,3 +1,6 @@
+from statistics import median
+
+
 def process_input(file: str) -> list[int]:
     with open(file) as f:
         data = sorted(list(map(int, f.read().split(","))))
@@ -6,11 +9,19 @@ def process_input(file: str) -> list[int]:
 
 def part_one(filename: str) -> int:
     data = process_input(input_file)
-    median = data[len(data) // 2]
-    fuel_cost = sum([abs(x - median) for x in data])
+    return int(sum([abs(x - median(data)) for x in data]))
 
-    return fuel_cost
+
+def part_two(filename: str) -> int:
+    data = process_input(input_file)
+    return min([sum([fuel_cost(x, r) for x in data]) for r in range(min(data), max(data) + 1)])
+
+
+def fuel_cost(start, end: int) -> int:
+    dist = abs(start - end)
+    return int(dist * (dist + 1) / 2)
 
 
 input_file = "./input.txt"
 print(f"Part One: {part_one(input_file)}")
+print(f"Part Two: {part_two(input_file)}")
