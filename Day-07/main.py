166692b8999199a3bbc4620c2d05db46f58821fb
Pat-Francis <github@pmfrancis.com>
2022-01-01 01:18:30 +0000

    Day 7 part 1 completed

---
+++ Day-07/main.py
@@ -0,0 +1,16 @@
+def process_input(file: str):
+    with open(file) as f:
+        data = sorted(list(map(int, f.read().split(","))))
+        return data
+
+
+def part_one(filename: str) -> int:
+    data = process_input(input_file)
+    median = data[len(data) // 2]
+    fuel_cost = sum([abs(x - median) for x in data])
+
+    return fuel_cost
+
+
+input_file = "./input.txt"
+print(f"Part One: {part_one(input_file)}")
