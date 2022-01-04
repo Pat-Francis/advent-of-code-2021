d2b094f10598fdda171ef5b5a688bf8dcbb6147d
Pat-Francis <github@pmfrancis.com>
2022-01-04 22:04:57 +0000

    Day 9 part 2 in progress

--- Day-09/main.py
+++ Day-09/main.py
@@ -3,61 +3,37 @@ def process_input(file: str) -> list[list[int]]:
         return [list(map(int, line)) for line in f.read().split()]
 
 
+def is_low_point(array: list[list[int]], x: int, y: int) -> bool:
+    indices = []
+    adjacency_matrix = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    for dx, dy in adjacency_matrix:
+        if 0 <= x + dx < len(array) and 0 <= y + dy < len(array[x]):
+            indices.append(array[x + dx][y + dy])
+
+    return array[x][y] < min(indices)
+
+
 def part_one(filename: str) -> int:
     data = process_input(filename)
-    adjacency_matrix = [(0, -1), (0, 1), (1, 0), (-1, 0)]
     risk_level = 0
 
     for x in range(len(data)):
         for y in range(len(data[x])):
-            indices = []
-
-            for dx, dy in adjacency_matrix:
-                if 0 <= x + dx < len(data) and 0 <= y + dy < len(data[x]):
-                    indices.append((x + dx, y + dy))
-
-            adjacent_values = [data[i][j] for (i, j) in indices]
-            current_value = data[x][y]
-
-            if all([current_value < i for i in adjacent_values]):
-                risk_level += 1 + current_value
+            if is_low_point(data, x, y):
+                risk_level += 1 + data[x][y]
 
     return risk_level
 
 
-def part_two(filename: str) -> list[tuple]:
+def part_two(filename: str):
     data = process_input(filename)
-    adjacency_matrix = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-    lowest_points = []
 
     for x in range(len(data)):
         for y in range(len(data[x])):
-            indices = []
-
-            for dx, dy in adjacency_matrix:
-                if 0 <= x + dx < len(data) and 0 <= y + dy < len(data[x]):
-                    indices.append((x + dx, y + dy))
-
-            adjacent_values = [data[i][j] for (i, j) in indices]
-            current_value = data[x][y]
-
-            if all([current_value < i for i in adjacent_values]):
-                lowest_points.append((x, y))
-
-    basin_sizes = []
-
-    for point in lowest_points:
-        basin = []
-        indices_to_check = [point]
-        print(indices_to_check)
-        for x, y in indices_to_check:
-            print(f"Index: {x, y}")
-            for dx, dy in adjacency_matrix:
-                if 0 <= x + dx < len(data) and 0 <= y + dy < len(data[x]) and data[x + dx][y + dy] < 9:
-                    print(f"check 2: {x + dx} - {y + dy}")
+            if is_low_point(data, x, y):
+                pass
 
 
-
-input_file = "./test_input.txt"
+input_file = "./input.txt"
 print(f"Part One: {part_one(input_file)}")
 print(f"Part Two: {part_two(input_file)}")
