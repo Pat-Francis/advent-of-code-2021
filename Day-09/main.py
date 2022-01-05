c6c237f89ccb281dbe6c3fbe6f08123d74add443
Pat-Francis <github@pmfrancis.com>
2022-01-05 21:34:57 +0000

    Day 9 completed

--- Day-09/main.py
+++ Day-09/main.py
@@ -4,13 +4,32 @@ def process_input(file: str) -> list[list[int]]:
 
 
 def is_low_point(array: list[list[int]], x: int, y: int) -> bool:
-    indices = []
+    adjacent = []
     adjacency_matrix = [(0, -1), (0, 1), (1, 0), (-1, 0)]
     for dx, dy in adjacency_matrix:
         if 0 <= x + dx < len(array) and 0 <= y + dy < len(array[x]):
-            indices.append(array[x + dx][y + dy])
+            adjacent.append(array[x + dx][y + dy])
 
-    return array[x][y] < min(indices)
+    return array[x][y] < min(adjacent)
+
+
+def calculate_basins(visited: set, array: list[list[int]], x, y):
+    adjacent = []
+    adjacency_matrix = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+
+    if (x, y) not in visited:
+        visited.add((x, y))
+
+    for dx, dy in adjacency_matrix:
+        if 0 <= x + dx < len(array) and 0 <= y + dy < len(array[x]) and array[x + dx][y + dy] != 9:
+            # print(x + dx, y + dy)
+            adjacent.append((x + dx, y + dy))
+
+    for (x, y) in adjacent:
+        if (x, y) not in visited:
+            calculate_basins(visited, array, x, y)
+    else:
+        return len(visited)
 
 
 def part_one(filename: str) -> int:
@@ -27,11 +46,21 @@ def part_one(filename: str) -> int:
 
 def part_two(filename: str):
     data = process_input(filename)
-
+    lowest_points = []
+    basin_sizes = []
     for x in range(len(data)):
         for y in range(len(data[x])):
             if is_low_point(data, x, y):
-                pass
+                lowest_points.append([x, y])
+
+    for point in lowest_points:
+        basin = set()
+
+        basin_size = calculate_basins(basin, data, point[0], point[1])
+        basin_sizes.append(basin_size)
+
+    basin_sizes.sort()
+    return basin_sizes[-1] * basin_sizes[-2] * basin_sizes[-3]
 
 
 input_file = "./input.txt"
