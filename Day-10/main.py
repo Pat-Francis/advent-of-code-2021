f3331276351d84203d8e72de072d0c2f3bb888c9
Pat-Francis <github@pmfrancis.com>
2022-01-06 21:30:54 +0000

    Day 10 part 1 complete

---
+++ Day-10/main.py
@@ -0,0 +1,34 @@
+points_dict = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+
+
+def process_input(file: str):
+    with open(file) as f:
+        return [line for line in f.read().split()]
+
+
+def part_one(filename: str):
+    data = process_input(filename)
+    openers = ['(', '[', '{', '<']
+    closers = [')', ']', '}', '>']
+    score = 0
+
+    for line in data:
+        open_chars = []
+        for char in line:
+            if char in openers:
+                open_chars.append(char)
+            elif open_chars[-1] == openers[closers.index(char)]:
+                open_chars = open_chars[:-1]
+            else:
+                score += points_dict.get(char)
+                break
+    return score
+
+
+input_file = "./input.txt"
+print(f"Part One: {part_one(input_file)}")
