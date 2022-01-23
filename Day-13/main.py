a9c6eb093552ead8b298e2a2ae650f5583b24fa2
Pat-Francis <github@pmfrancis.com>
2022-01-23 20:17:06 +0000

    Day 13 part 1 complete

--- Day-13/main.py
+++ Day-13/main.py
@@ -2,8 +2,8 @@ def process_input(file: str):
     with open(file) as f:
         dots, folds = f.read().split("\n\n")
 
-    dots = sorted([list(map(int, item.split(","))) for item in dots.split("\n")])
-
+    dots = [list(map(int, item.split(","))) for item in dots.split("\n")]
+    print(dots)
     x_max = max((x for x, y in dots))
     y_max = max((y for x, y in dots))
 
@@ -14,17 +14,35 @@ def process_input(file: str):
     return dots, x_max, y_max, folds
 
 
-def fold_paper(dots, folds):
-    if folds[0] == "x":
-        print("x")
-    else:
-        print("y")
+def fold_paper(dots, x_max, y_max, axis, value):
+    new_dots = []
+    if axis == "x":
+        for x, y in dots:
+            if x > value:
+                new_dots.append((2 * value - x, y))
+            else:
+                new_dots.append((x, y))
+        x_max = value - 1
+    elif axis == "y":
+        for x, y in dots:
+            if y > value:
+                new_dots.append((x, 2 * value - y))
+            else:
+                new_dots.append((x, y))
+        y_max = value - 1
+    dots = new_dots
+
+    return dots, x_max, y_max
 
 
 def part_one(filename: str):
     dots, x_max, y_max, folds = process_input(filename)
-    fold_paper(dots, folds[0])
+    for axis, value in folds:
+        dots, x_max, y_max = fold_paper(dots, x_max, y_max, axis, value)
+        break  # only first fold is required for part one
+
+    return len(set(dots))
 
 
-input_file = "./test_input.txt"
+input_file = "./input.txt"
 print(f"Part One: {part_one(input_file)}")
