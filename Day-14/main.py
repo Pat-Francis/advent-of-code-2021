cb28940eaff582d27b248d736a52c651951da345
Pat-Francis <github@pmfrancis.com>
2022-01-26 20:36:20 +0000

    Day 14 integrate insert_element function into part_one

--- Day-14/main.py
+++ Day-14/main.py
@@ -15,30 +15,23 @@ def process_input(file: str):
     return template, data
 
 
-def insert_element(template: str, data: defaultdict, iterations: int) -> int:
-    if iterations > 0:
+def part_one(filename: str):
+    template, data = process_input(filename)
+
+    for j in range(10):
         new_template = ""
         for i in range(len(template) - 1):
             pair = f"{template[i]}{template[i + 1]}"
             insert = data[pair]
             new_template += f"{template[i]}{insert}"
-        new_template += f"{template[-1]}" # adds last character of template to new_template
+        new_template += f"{template[-1]}"  # adds last character of template to new_template
         template = new_template
-        iterations -= 1
-
-        return insert_element(template, data, iterations)
     else:
         counts = Counter(template)
         most_common = counts.most_common()[0][1]
         least_common = counts.most_common()[-1][1]
 
         return most_common - least_common
-
-
-def part_one(filename: str):
-    template, data = process_input(filename)
-    update = insert_element(template, data, 10)
-    return update
 
 
 input_file = "./input.txt"
